ca5ed7398da4b37c4fc00f1618a46b6ac4775ead
mbrayton27 <mbrayton27@gmail.com>
2021-03-10 21:19:04 -0800

    Completed second test. Checks if year satisfies divide by 4 and 100 but not 400.

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 import unittest
 import main1
 
-class TestCase(unittest.TestCase):
+class TestCase1(unittest.TestCase):
     def test1(self):
         self.assertEqual(main1.print_count(400), 'That year is a leap year')
+
+    def test2(self):
+        self.assertEqual(main1.print_count(200), 'That year is NOT a leap year')
 
 if __name__ == '__main__':
     unittest.main()
