e24b70fb0208af97679ee5f50be50c450f372155
mbrayton27 <mbrayton27@gmail.com>
2021-03-10 21:24:12 -0800

    Third test written. Checks to see code satisfies being divisible by 4 but not 100 or 400.

--- test.py
+++ test.py
@@ -8,5 +8,8 @@ class TestCase1(unittest.TestCase):
     def test2(self):
         self.assertEqual(main1.print_count(200), 'That year is NOT a leap year')
 
+    def test3(self):
+        self.assertEqual(main1.print_count(8), 'That year is a leap year')
+
 if __name__ == '__main__':
     unittest.main()
